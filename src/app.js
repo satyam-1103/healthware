7aecde7ee7d158e02cca7077898bc24200aa52c9
Satyam Srivastava <satyamsrivastav435@gmail.com>
2022-02-10 14:14:57 +0530

    added file

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+app.use('/css',express.static(__dirname + 'public/css'));
+app.use('/img',express.static(__dirname + 'public/img'));
+app.get("/",(req,res) => {
+    res.sendFile("/index.html");
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
